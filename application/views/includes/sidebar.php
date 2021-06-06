e0576efe649750f031a214c90e84234ca5432ad5
Can Gultekin <68272942+cangultekin@users.noreply.github.com>
2021-06-07 00:59:03 +0300

    Update sidebar.php

--- application/views/includes/sidebar.php
+++ application/views/includes/sidebar.php
@@ -1,5 +1,5 @@
 <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
-    <a class="navbar-brand ps-3" href="<?php echo base_url(); ?>">Tepe Güvenlik</a>
+    <a class="navbar-brand ps-3" href="https://www.tepeguvenlik.com.tr/" target="_blank">Tepe Güvenlik</a>
     <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!"><i
             class="fas fa-bars"></i></button>
     <form class="d-none d-md-inline-block form-inline ms-auto me-0 me-md-3 my-2 my-md-0">
